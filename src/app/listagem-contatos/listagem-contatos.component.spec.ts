18d38df768e4a04b597b1f599fa42df2d4b87979
Igor Cemim <igor@cemim.com.br>
2020-12-05 20:46:35 -0300

    Angular CLI atualizado para a versão 11

--- src/app/listagem-contatos/listagem-contatos.component.spec.ts
+++ src/app/listagem-contatos/listagem-contatos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ListagemContatosComponent } from './listagem-contatos.component';
 
@@ -6,7 +6,7 @@ describe('ListagemContatosComponent', () => {
   let component: ListagemContatosComponent;
   let fixture: ComponentFixture<ListagemContatosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ListagemContatosComponent ]
     })
--- src/app/manutencao-contatos/manutencao-contatos.component.spec.ts
+++ src/app/manutencao-contatos/manutencao-contatos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ManutencaoContatosComponent } from './manutencao-contatos.component';
 
@@ -6,7 +6,7 @@ describe('ManutencaoContatosComponent', () => {
   let component: ManutencaoContatosComponent;
   let fixture: ComponentFixture<ManutencaoContatosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ManutencaoContatosComponent ]
     })
